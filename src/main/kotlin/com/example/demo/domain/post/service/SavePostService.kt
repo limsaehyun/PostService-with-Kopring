6e313d4fafa02df8a13e220065f4c95464891e90
임세현 <sh00710000@gmail.com>
2022-07-10 20:32:16 +0900

    feat: save post service

---
+++ src/main/kotlin/com/example/demo/domain/post/service/SavePostService.kt
@@ -0,0 +1,28 @@
+package com.example.demo.domain.post.service
+
+import com.example.demo.domain.post.payload.request.SavePostRequest
+import com.example.demo.domain.post.payload.response.SavePostResponse
+import com.example.demo.domain.post.mapper.toEntity
+import com.example.demo.domain.post.repository.PostRepository
+import com.example.demo.domain.post.utils.Message
+import com.example.demo.global.payload.BaseResponse
+import org.springframework.stereotype.Service
+
+@Service
+class SavePostService(
+        val postRepository: PostRepository
+) {
+
+    fun savePost(postRequest: SavePostRequest): BaseResponse<SavePostResponse> {
+
+        val savedPost = postRepository.save(postRequest.toEntity())
+
+        val responseContent = SavePostResponse(savedPost.id)
+
+        return BaseResponse(
+                200,
+                Message.SAVE_POST_SUCCESS_MESSAGE,
+                responseContent
+        )
+    }
+}
