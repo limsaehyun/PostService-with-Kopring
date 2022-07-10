b1fe29d5d87c1485abf658a017d97a609f287156
임세현 <sh00710000@gmail.com>
2022-07-10 20:31:58 +0900

    feat: message

---
+++ src/main/kotlin/com/example/demo/domain/post/utils/Message.kt
@@ -0,0 +1,9 @@
+package com.example.demo.domain.post.utils
+
+object Message {
+    const val SAVE_POST_SUCCESS_MESSAGE = "Post Save Success!"
+    const val GET_POST_SUCCESS_MESSAGE = "Post Get Success!"
+    const val GET_POST_LIST_SUCCESS_MESSAGE = "Post List Get Success!"
+    const val UPDATE_POST_SUCCESS_MESSAGE = "Post Update Success!"
+    const val DELETE_POST_SUCCESS_MESSAGE = "Post Delete Success!"
+}
