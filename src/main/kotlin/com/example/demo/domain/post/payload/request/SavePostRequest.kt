378e9988638fae2bd049a7fe925a0aaab8539882
임세현 <sh00710000@gmail.com>
2022-07-10 20:32:36 +0900

    feat: save post request & delete post response

---
+++ src/main/kotlin/com/example/demo/domain/post/payload/request/SavePostRequest.kt
@@ -0,0 +1,6 @@
+package com.example.demo.domain.post.payload.request
+
+data class SavePostRequest(
+        val title: String,
+        val content: String
+)
---
+++ src/main/kotlin/com/example/demo/domain/post/payload/response/DeletePostResponse.kt
@@ -0,0 +1,5 @@
+package com.example.demo.domain.post.payload.response
+
+class DeletePostResponse(
+        val id: Int
+)
