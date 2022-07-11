faff4d363c7eb3a76b417e568d01272c47d5352e
임세현 <sh00710000@gmail.com>
2022-07-11 09:19:25 +0900

    feat: post request valid

--- src/main/kotlin/com/example/demo/domain/post/controller/PostController.kt
+++ src/main/kotlin/com/example/demo/domain/post/controller/PostController.kt
@@ -7,6 +7,8 @@ import com.example.demo.domain.post.payload.response.DeletePostResponse
 import com.example.demo.domain.post.payload.response.SavePostResponse
 import com.example.demo.domain.post.service.*
 import com.example.demo.global.payload.BaseResponse
+import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/post")
@@ -26,13 +29,14 @@ class PostController(
         private val patchPostService: PatchPostService
 ) {
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    fun savePost(@RequestBody request: SavePostRequest): BaseResponse<SavePostResponse> {
+    fun savePost(@RequestBody @Validated request: SavePostRequest): BaseResponse<SavePostResponse> {
         return savePostService.savePost(request)
     }
 
     @GetMapping("/{post-id}")
-    fun getPost(@PathVariable(name = "post-id") id: Int): BaseResponse<GetPostResponse> {
+    fun getPost(@PathVariable("post-id") id: Int): BaseResponse<GetPostResponse> {
         return getPostService.getPost(id)
     }
 
@@ -42,12 +46,12 @@ class PostController(
     }
 
     @PatchMapping("/{post-id}")
-    fun patchPost(@PathVariable(name = "post-id") id: Int, @RequestBody patchPostRequest: PatchPostRequest): BaseResponse<Unit> {
+    fun patchPost(@PathVariable("post-id") id: Int, @RequestBody @Validated patchPostRequest: PatchPostRequest): BaseResponse<Unit> {
         return patchPostService.patchPost(id, patchPostRequest)
     }
 
     @DeleteMapping("/{post-id}")
-    fun deletePost(@PathVariable(name = "post-id") id: Int): BaseResponse<DeletePostResponse> {
+    fun deletePost(@PathVariable("post-id") id: Int): BaseResponse<DeletePostResponse> {
         return deletePostService.deletePost(id)
     }
 }
--- src/main/kotlin/com/example/demo/domain/post/payload/request/SavePostRequest.kt
+++ src/main/kotlin/com/example/demo/domain/post/payload/request/SavePostRequest.kt
@@ -1,6 +1,14 @@
 package com.example.demo.domain.post.payload.request
 
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
 data class SavePostRequest(
+        @field:NotNull(message = "title must not null")
+        @field:Size(min = 3, message = "제목을 3글자 이상 입력해주세요.")
         val title: String,
+
+        @field:NotNull(message = "content must not null")
+        @field:Size(min = 3, message = "내용을 3글자 이상 입력해주세요.")
         val content: String
 )
