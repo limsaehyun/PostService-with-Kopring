607e54c00e684df12c5a71fcdc177d1b0b83bec0
임세현 <sh00710000@gmail.com>
2022-07-16 20:24:05 +0900

    feat: login controller & payload

--- src/main/kotlin/com/example/demo/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/example/demo/domain/user/controller/UserController.kt
@@ -1,8 +1,11 @@
 package com.example.demo.domain.user.controller
 
+import com.example.demo.domain.user.payload.request.SignInRequest
 import com.example.demo.domain.user.payload.request.SignUpRequest
+import com.example.demo.domain.user.service.SignInService
 import com.example.demo.domain.user.service.SignUpService
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,12 +16,18 @@ import javax.validation.Valid
 @RequestMapping("/user")
 @RestController
 class UserController(
-        private val signUpService: SignUpService
+        private val signUpService: SignUpService,
+        private val signInService: SignInService
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun save(@RequestBody @Valid request: SignUpRequest) {
         signUpService.save(request)
+    }
+
+    @GetMapping("/token")
+    fun login(@RequestBody @Valid request: SignInRequest) {
+        signInService.login(request);
     }
 }
